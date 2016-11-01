82cdf2486557310ad85a95647cf5a8b94d16de40
Alastair Hughes <hobbitalastair@gmail.com>
2016-11-01 20:55:16 +1300

    Rewrite to use http.* for errors, fix function style

--- backend.go
+++ backend.go
@@ -1,7 +1,10 @@
-/*	MEL app backend
+/*
+MEL app backend.
 
-	Author:		Alastair Hughes
-	Contact:	<hobbitalastair at yandex dot com>
+
+ 
+Author:		Alastair Hughes
+Contact:	<hobbitalastair at yandex dot com>
 */
 
 package main
@@ -13,27 +16,29 @@ import (
 )
 
 // Handle a single HTTP request.
-func MELHandler(writer http.ResponseWriter, request *http.Request) {
+func Handle(writer http.ResponseWriter, request *http.Request) {
 
 	fmt.Printf("Handling request for %q\n", html.EscapeString(request.URL.Path))
 
+	// Authenticate.
 	name, password, ok := request.BasicAuth()
 	if !ok {
-		writer.WriteHeader(401) // Auth required
+		http.Error(writer, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 		return
 	}
 	if name != "" || password != "" {
 		// TODO: Implement checking against the db.
-		writer.WriteHeader(403) // Invalid auth
+		http.Error(writer, http.StatusText(http.StatusForbidden), http.StatusForbidden)
 		return
 	}
-
 	fmt.Fprintf(writer, "%q: authenticated as %s\n",
 		html.EscapeString(request.URL.Path), name)
+
+	// Parse the URL and return the corresponding value.
 }
 
 func main() {
-	http.ListenAndServe(":8080", http.HandlerFunc(MELHandler))
+	http.ListenAndServe(":8080", http.HandlerFunc(Handle))
 }
 
 // vim: sw=4 ts=4 noexpandtab
