51e8178756a98cd08afca9e167c830435b1eede7
Alastair Hughes <hobbitalastair@gmail.com>
2016-11-24 14:19:53 +1300

    Fixed deletion authentication

--- backend.go
+++ backend.go
@@ -108,7 +108,8 @@ func authenticateUser(writer http.ResponseWriter, fail func(int), request *http.
 func authenticateRequest(request *http.Request, resource Resource) (ok bool) {
 	return ((request.Method == http.MethodGet) && (resource.Permissions()&Get != 0)) ||
 		((request.Method == http.MethodPut) && (resource.Permissions()&Set != 0)) ||
-		((request.Method == http.MethodPost) && (resource.Permissions()&Create != 0))
+		((request.Method == http.MethodPost) && (resource.Permissions()&Create != 0)) ||
+		((request.Method == http.MethodDelete) && (resource.Permissions()&Delete != 0))
 }
 
 // handle a single HTTP request.
