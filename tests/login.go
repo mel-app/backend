6aeba7a0d9918729887708b0cd86e10bb2a52f55
Alastair Hughes <hobbitalastair@gmail.com>
2017-01-19 11:41:31 +1300

    Test for both is a manager and is not

--- tests/login.go
+++ tests/login.go
@@ -8,9 +8,12 @@ Contact:	<hobbitalastair at yandex dot com>
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/mel-app/backend/src"
 )
 
 var loginUrl = url + "login"
@@ -39,6 +42,12 @@ var loginTests = []Test{
 		Method: "POST", URL: loginUrl, Status: http.StatusForbidden,
 		SetAuth: setWrongPassword,
 	},
+	Test{
+		Name:	"login:MakeManager",
+		Method: "GET", URL: loginUrl, Status: http.StatusOK,
+		Pre:	makeManager,
+		CheckBody: checkManager,
+	},
 }
 
 type login struct {
@@ -53,6 +62,21 @@ func setNilAuth(r *http.Request) {
 // setWrongPassword provides an invalid password.
 func setWrongPassword(r *http.Request) {
 	r.SetBasicAuth(defaultUser, "some other password")
+}
+
+// makeManager makes the default user a manager.
+func makeManager(db *sql.DB) error {
+	return backend.NewDB(db).SetIsManager(defaultUser, true)
+}
+
+// checkManager checks that the manager flag is set.
+func checkManager(dec *json.Decoder) error {
+	login := login{false}
+	dec.Decode(&login)
+	if login.Manager != true {
+		return fmt.Errorf("Setting a manager did not work!")
+	}
+	return nil
 }
 
 // checkNotManager checks that the manager flag is not set.
